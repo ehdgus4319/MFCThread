df4fe0a69b04d36e342440abc935dd82dfa06d13
동현 김 <ehdgus0512@gmail.com>
2022-07-08 11:15:04 +0900

    초기 커밋.

---
+++ MFCThread/MFCThreadDlg.h
@@ -0,0 +1,38 @@
+﻿
+// MFCThreadDlg.h: 헤더 파일
+//
+
+#pragma once
+#define MYMSG WM_USER + 1
+
+// CMFCThreadDlg 대화 상자
+class CMFCThreadDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCThreadDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCTHREAD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+protected:
+	afx_msg LRESULT OnMymsg(WPARAM wParam, LPARAM lParam);
+};
